f821472e4674a686bdde02fb3e9d93245dccf4d1
Giulio Camuffo <giuliocamuffo@gmail.com>
2013-10-10 16:38:54 +0200

    client: add italian translation for the common qml components

---
+++ src/client/translations/it_IT.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>ElementsChooser</name>
+    <message>
+        <location filename="../qml/ElementsChooser.qml" line="61"/>
+        <source>Style:</source>
+        <translation>Stile:</translation>
+    </message>
+</context>
+</TS>
