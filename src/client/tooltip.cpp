5463630d93916d9f89d642827389d2b77cf9aa64
Giulio Camuffo <giuliocamuffo@gmail.com>
2013-12-13 17:20:08 +0100

    Improve the placing of Tooltips

--- src/client/tooltip.cpp
+++ src/client/tooltip.cpp
@@ -25,6 +25,7 @@
 #include "tooltip.h"
 #include "client.h"
 #include "grab.h"
+#include "element.h"
 
 static const int a = qmlRegisterType<ToolTip>("Orbital", 1, 0, "ToolTip");
 
@@ -82,6 +83,12 @@ void ToolTip::hide()
 
 void ToolTip::doShow()
 {
+    Element *e = Element::fromItem(this);
+    if (!e) {
+        qWarning() << "ToolTip::doShow(): No parent element found, bailing out.";
+        return;
+    }
+
     QQuickWindow *w = parentItem()->window();
     QPointF pos = parentItem()->mapToScene(QPointF(0, 0));
     m_window = new QQuickWindow();
@@ -90,8 +97,25 @@ void ToolTip::doShow()
     m_window->setScreen(w->screen());
 
     // TODO: Better placement, maybe by adding some protocol
-    m_window->setX(pos.x());
-    m_window->setY(pos.y() + parentItem()->height() + 5);
+    switch (e->location()) {
+        case Element::Location::TopEdge:
+        case Element::Location::Floating:
+            m_window->setX(pos.x());
+            m_window->setY(pos.y() + parentItem()->height() + 5);
+            break;
+        case Element::Location::LeftEdge:
+            m_window->setX(pos.x() + parentItem()->width() + 5);
+            m_window->setY(pos.y());
+            break;
+        case Element::Location::RightEdge:
+            m_window->setX(pos.x() - m_content->width() - 5);
+            m_window->setY(pos.y());
+            break;
+        case Element::Location::BottomEdge:
+            m_window->setX(pos.x());
+            m_window->setY(pos.y() - m_content->height() - 5);
+            break;
+    }
 
     m_window->setWidth(m_content->width());
     m_window->setHeight(m_content->height());
