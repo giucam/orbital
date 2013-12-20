f5559d4b062121343964ce01d8d46cb78a2b3c05
Giulio Camuffo <giuliocamuffo@gmail.com>
2013-12-20 14:28:30 +0100

    Map the left and right arrow keys

--- src/client/compositorsettings.cpp
+++ src/client/compositorsettings.cpp
@@ -106,58 +106,57 @@ static bool modFromString(const QString &t, nuclear_settings_modifier *mod)
 }
 
 struct Key {
-    int c;
+    const char *c;
     int k;
 };
 
 static Key keymap[] = {
-    { 'a', KEY_A },
-    { 'b', KEY_B },
-    { 'c', KEY_C },
-    { 'd', KEY_D },
-    { 'e', KEY_E },
-    { 'f', KEY_F },
-    { 'g', KEY_G },
-    { 'h', KEY_H },
-    { 'i', KEY_I },
-    { 'j', KEY_J },
-    { 'k', KEY_K },
-    { 'l', KEY_L },
-    { 'm', KEY_M },
-    { 'n', KEY_N },
-    { 'o', KEY_O },
-    { 'p', KEY_P },
-    { 'q', KEY_Q },
-    { 'r', KEY_R },
-    { 's', KEY_S },
-    { 't', KEY_T },
-    { 'v', KEY_V },
-    { 'u', KEY_U },
-    { 'w', KEY_W },
-    { 'x', KEY_X },
-    { 'y', KEY_Y },
-    { 'z', KEY_X },
-    { '0', KEY_0 },
-    { '1', KEY_1 },
-    { '2', KEY_2 },
-    { '3', KEY_3 },
-    { '4', KEY_4 },
-    { '5', KEY_5 },
-    { '6', KEY_6 },
-    { '7', KEY_7 },
-    { '8', KEY_8 },
-    { '9', KEY_9 }
+    { "a", KEY_A },
+    { "b", KEY_B },
+    { "c", KEY_C },
+    { "d", KEY_D },
+    { "e", KEY_E },
+    { "f", KEY_F },
+    { "g", KEY_G },
+    { "h", KEY_H },
+    { "i", KEY_I },
+    { "j", KEY_J },
+    { "k", KEY_K },
+    { "l", KEY_L },
+    { "m", KEY_M },
+    { "n", KEY_N },
+    { "o", KEY_O },
+    { "p", KEY_P },
+    { "q", KEY_Q },
+    { "r", KEY_R },
+    { "s", KEY_S },
+    { "t", KEY_T },
+    { "v", KEY_V },
+    { "u", KEY_U },
+    { "w", KEY_W },
+    { "x", KEY_X },
+    { "y", KEY_Y },
+    { "z", KEY_X },
+    { "0", KEY_0 },
+    { "1", KEY_1 },
+    { "2", KEY_2 },
+    { "3", KEY_3 },
+    { "4", KEY_4 },
+    { "5", KEY_5 },
+    { "6", KEY_6 },
+    { "7", KEY_7 },
+    { "8", KEY_8 },
+    { "9", KEY_9 },
+    { "left", KEY_LEFT },
+    { "right", KEY_RIGHT }
 };
 
 static bool keyFromString(const QString &t, int *key)
 {
-    if (t.length() == 1) {
-        char c = qPrintable(t)[0];
-        for (unsigned int i = 0; i < sizeof(keymap) / sizeof(Key); ++i) {
-            if (keymap[i].c == c) {
-                *key = keymap[i].k;
-                return true;
-            }
+    for (unsigned int i = 0; i < sizeof(keymap) / sizeof(Key); ++i) {
+        if (keymap[i].c == t) {
+            *key = keymap[i].k;
+            return true;
         }
     }
 
