23c9bcf1576ebabd4989679dd18bdcc1586a00f0
Giulio Camuffo <giuliocamuffo@gmail.com>
2013-12-21 12:07:53 +0100

    Export QT_QPA_PLATFORM and XDG_CURRENT_DESKTOP from the 'orbital' script

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -26,9 +26,7 @@
 int main(int argc, char *argv[])
 {
     // Force Wayland platform plugin
-    setenv("QT_QPA_PLATFORM", "wayland", 1);
     setenv("QT_MESSAGE_PATTERN", "[%{type}] %{message}", 0);
-    setenv("XDG_CURRENT_DESKTOP", "orbital", 1);
 
     QApplication app(argc, argv);
     Client client;
