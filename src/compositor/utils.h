34e321f3902ba857cf067956ff361f8fdc91325f
Giulio Camuffo <giuliocamuffo@gmail.com>
2014-08-25 19:00:41 +0300

    compositor: add license headers

--- src/compositor/utils.h
+++ src/compositor/utils.h
@@ -1,18 +1,20 @@
 /*
  * Copyright 2013-2014 Giulio Camuffo <giuliocamuffo@gmail.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This file is part of Orbital
  *
- * This library is distributed in the hope that it will be useful,
+ * Orbital is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Orbital is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with Orbital.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ORBITAL_UTILS_H
